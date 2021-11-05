6b362956df401a9ea9487b26abe21a79cda216ef
Naaaggi <naguigh23@gmail.com>
2021-11-05 01:31:15 +0100

    init

---
+++ script.js
@@ -0,0 +1,86 @@
+function validateForm() {
+    var username = document.forms["contactForm"]["username"].value;
+    var firstname = document.forms["contactForm"]["firstname"].value;
+    var lastname = document.forms["contactForm"]["lastname"].value;
+    var email = document.forms["contactForm"]["email"].value;
+    var password = document.forms["contactForm"]["password"].value;
+    var cpassword = document.forms["contactForm"]["password-confirm"].value;
+    
+    if (username == null || username == "") {
+        alert("Please insert a username");
+        return false;
+    }
+    
+    else if (firstname == null || firstname == "") {
+        alert("First Name must be filled out");
+        return false;
+    } else if (lastname == null || lastname == "") {
+        alert("Last Name must be filled out");
+        return false;
+}else if (email == null || email == "") {
+    alert("Email must be filled out");
+    return false;
+
+}
+else if (password == null || password == "") {
+    alert("Password must be filled out");
+    return false;
+
+}
+else if (password !== cpassword ) {
+    alert("The password doesn't match");
+    return false;
+
+}
+else return true;
+}
+
+var leftattempts=3
+
+ function validateAdmin(){
+
+    var username= document.forms["contactForm"]["username"].value;
+    var password= document.forms["contactForm"]["password"].value;
+    var submitLogin = document.getElementById("login");
+    var e = document.getElementById("formchoice");
+    var formaction = e.options[e.selectedIndex].value;
+
+
+    switch (formaction) {
+        case "./staff.html":
+            if (username == null || username == "") {
+                alert("Username must be filled out");
+                return false;
+            }
+            
+            else if (password == null || password == "") {
+                alert("Passowrd must be filled out");
+                return false;
+            }
+            else{document.forms["contactForm"].action = "./staff.html";
+            return true;}
+
+        
+        break;
+        case "./admin.html":
+  
+
+    if (username !="Admin" || password != "Admin") {
+        leftattempts--;
+        alert("Login failed, Incorrect usename or password, you still have " + leftattempts + " attempt(s).");
+        if(leftattempts==0){
+        alert("The username or password was incorrect 3 times, the login button is now going to be disabled");
+        submitLogin.disabled = true;
+        
+    }
+    return false;
+}
+else{
+    document.forms["contactForm"].action = "./admin.html";
+
+}
+break;
+
+        
+}
+ } 
---
+++ student.js
@@ -0,0 +1,135 @@
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validateSI() && validateFN() &&  validateDM()  && validateEM())  {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["StudentId"] = document.getElementById("StudentId").value;
+    formData["firstname"] = document.getElementById("firstname").value;
+    formData["lastname"] = document.getElementById("lastname").value;
+    formData["dob"] = document.getElementById("dob").value;
+    formData["gender"] = document.querySelector('input[name="gender"]:checked').value;
+    formData["department"] = document.getElementById("department").value;
+    formData["email"] = document.getElementById("email").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.StudentId;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.firstname;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.lastname;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.dob;
+    cell5 = newRow.insertCell(4);
+    cell5.innerHTML = data.gender;
+    cell6 = newRow.insertCell(5);
+    cell6.innerHTML = data.department;
+    cell7 = newRow.insertCell(6);
+    cell7.innerHTML = data.email;
+    cell8 = newRow.insertCell(7);
+    cell8.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("StudentId").value = "";
+    document.getElementById("firstname").value = "";
+    document.getElementById("lastname").value = "";
+    document.getElementById("dob").value = "";
+    document.getElementById("gender").value = "";
+    document.getElementById("department").value = "";
+    document.getElementById("email").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("StudentId").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("firstname").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("lastname").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("dob").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("gender").value = selectedRow.cells[4].innerHTML;
+    document.getElementById("department").value = selectedRow.cells[5].innerHTML;
+    document.getElementById("email").value = selectedRow.cells[6].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.StudentId;
+    selectedRow.cells[1].innerHTML = formData.firstname;
+    selectedRow.cells[2].innerHTML = formData.lastname;
+    selectedRow.cells[3].innerHTML = formData.dob;
+    selectedRow.cells[4].innerHTML = formData.gender;
+    selectedRow.cells[5].innerHTML = formData.department;
+    selectedRow.cells[6].innerHTML = formData.email;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+function validateSI() {
+    isValid = true;
+    if (document.getElementById("StudentId").value == "") {
+        isValid = false;
+        document.getElementById("StudentIdValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("StudentIdValidationError").classList.contains("hide"))
+            document.getElementById("StudentIdValidationError").classList.add("hide");
+    }
+    return isValid;
+}
+function validateFN() {
+    isValid = true;
+    if (document.getElementById("firstname").value == "") {
+        isValid = false;
+        document.getElementById("FirstNameValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("FirstNameValidationError").classList.contains("hide"))
+            document.getElementById("FirstNameValidationError").classList.add("hide");
+    }
+    return isValid;
+}
+function validateDM() {
+    isValid = true;
+    if (document.getElementById("department").value == "") {
+        isValid = false;
+        document.getElementById("DepartmentValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("DepartmentValidationError").classList.contains("hide"))
+            document.getElementById("DepartmentValidationError").classList.add("hide");
+    }
+    return isValid;
+}
+function validateEM() {
+    isValid = true;
+    if (document.getElementById("email").value == "") {
+        isValid = false;
+        document.getElementById("EmailValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("EmailValidationError").classList.contains("hide"))
+            document.getElementById("EmailValidationError").classList.add("hide");
+    }
+    return isValid;
+}
+
+
