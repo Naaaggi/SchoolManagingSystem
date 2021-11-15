f699c70bfd883aadfc5879d8ba020fc0973ed497
Naaaggi <naguigh23@gmail.com>
2021-11-15 23:12:54 +0100

    changed success.html and added a json file

---
+++ studentjson.js
@@ -0,0 +1,10 @@
+fetch("./students.json")
+.then(function(resp) {
+    return resp.json();
+})
+.then(function(data) {
+    console.log(data);
+    for (let i in data){
+    document.querySelector(".test").innerText += data[i].ID +' ';
+    }
+})
