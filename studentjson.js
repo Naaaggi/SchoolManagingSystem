30dbe6baee82387497e581e9add3984b048e01c1
Naaaggi <naguigh23@gmail.com>
2021-12-05 16:07:15 +0100

    added 150 students to filter by course

--- studentjson.js
+++ studentjson.js
@@ -1,10 +1,36 @@
-fetch("./students.json")
+
+function checkCourse(){
+var Table = document.getElementById("myTable")
+Table.innerHTML = ""
+fetch("students.json")
 .then(function(resp) {
-    return resp.json();
+    return resp.json()
 })
 .then(function(data) {
-    console.log(data);
-    for (let i in data){
-    document.querySelector(".test").innerText += data[i].ID;
+    var courseSelect = document.getElementById('course')
+    var value = courseSelect.options[courseSelect.selectedIndex].value
+    console.log(value)
+    var CS = data.filter(function (entry) {
+        return entry.Department === value
+    });
+    console.log(CS)
+    for(i in CS){
+        document.getElementById("myTable").innerHTML += `<tr>
+                                                        <td>${CS[i].ID}</td>
+                                                        <td>${CS[i].FirstName}</td>
+                                                        <td>${CS[i].LastName}</td>
+                                                        <td>${CS[i].Gender}</td>
+                                                        <td>${CS[i].DOB}</td>
+                                                        <td>${CS[i].Department}</td>
+                                                        <td>${CS[i].EmailID}</td>
+                                                        <td>${CS[i].JoiningDate}</td>
+                                                        </tr>`
     }
-})
+        
+
+
+    }
+)
+}
+
+
